6086d1118c7df0f4fad9f28134b603e275261681
Quan Vu <nongdenchet8435848@gmail.com>
2025-02-06 22:00:40 -0800

    tada

--- main.cpp
+++ main.cpp
@@ -5,21 +5,21 @@
 
 int main() {
     std::vector<std::string> inputFileNames;
-    inputFileNames.emplace_back("../InputFile.txt");
-    inputFileNames.emplace_back("../InputFile2.txt");
-    inputFileNames.emplace_back("../InputFile3.txt");
+    inputFileNames.emplace_back("./InputFile.txt");
+    inputFileNames.emplace_back("./InputFile2.txt");
+    inputFileNames.emplace_back("./InputFile3.txt");
 
     std::vector<std::string> compressedFileNames;
-    compressedFileNames.emplace_back("../CompressedFile.txt");
-    compressedFileNames.emplace_back("../CompressedFile2.txt");
-    compressedFileNames.emplace_back("../CompressedFile3.txt");
+    compressedFileNames.emplace_back("./CompressedFile.txt");
+    compressedFileNames.emplace_back("./CompressedFile2.txt");
+    compressedFileNames.emplace_back("./CompressedFile3.txt");
 
     for (int i = 0; i < inputFileNames.size(); i++) {
         std::string text;
         std::vector<char> symbol;
         std::vector<int> freq;
 
-        std::string inputFileName = inputFileNames[i];
+        const std::string& inputFileName = inputFileNames[i];
         std::ifstream inputFile(inputFileName);
         while (getline(inputFile, text)) {
             char c;
